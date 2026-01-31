4ad9e85cdeefafabfd77590f9f892f7b5fb1270b
mslthn <msulthon1909@gmail.com>
2026-01-31 11:34:32 +0700

    fix: fixing the menu dislay styling

--- src/components/MenuFrame.jsx
+++ src/components/MenuFrame.jsx
@@ -7,7 +7,6 @@ function MenuFrame(props) {
     title,
     description,
     showRating = false,
-    // rating = 5,
     ratingValue,
     oldPrice,
     price,
@@ -19,20 +18,20 @@ function MenuFrame(props) {
   const stars = [1, 2, 3, 4, 5];
 
   return (
-    <div className="relative items-center flex flex-col">
+    <div className="flex flex-col items-center">
       {showFlashSale && (
-        <h4 className="absolute top-3.5 left-3.5 bg-[#d00000] text-white text-lg font-bold font-[Plus_Jakarta_Sans] p-2.5 rounded-3xl z-2">
+        <h4 className="top-15 justify-self-start bg-[#d00000] text-white text-lg font-bold p-2 px-5 rounded-3xl z-3 relative">
           FLASH SALE!
         </h4>
       )}
-      <header className="relative z-2">
+      <header className=" z-2">
         <img src={image} alt={title} className="size-72 object-cover" />
       </header>
-      <div className="bg-white w-3xs h-3/4 p-2 -mt-15 relative z-2">
-        <h3 className="text-[22px] font-medium text-[#0b132a] font-[Plus_Jakarta_Sans]">
+      <div className="bg-white w-3xs h-3/4 p-2 -mt-15  z-2">
+        <h3 className="text-[22px] font-medium text-[#0b132a]">
           {title}
         </h3>
-        <p className="text-[#4f5665] text-sm font-normal font-[Plus_Jakarta_Sans]">
+        <p className="text-[#4f5665] text-sm font-normal">
           {description}
         </p>
         {showRating && (
@@ -41,28 +40,28 @@ function MenuFrame(props) {
               <img
                 key={star}
                 src="src/assets/icons/star.svg"
-                alt="icon-star"
-                className="mr-2.5"
+                alt="star rating"
+                className="mr-2"
               />
             ))}
-            <span className="text-[#4f5665] text-base font-normal font-[Plus_Jakarta_Sans]">
+            <span className="text-[#4f5665]">
               {ratingValue || `${rate}.0`}
             </span>
           </div>
         )}
         <div className="flex items-center">
           {oldPrice && (
-            <span className="text-[#d00000] text-xs font-medium font-[Plus_Jakarta_Sans] line-through mr-2.5">
+            <span className="text-[#d00000] text-xs font-medium line-through mr-2">
               {oldPrice}
             </span>
           )}
-          <span className="text-[#ff8906] text-xl font-medium font-[Plus_Jakarta_Sans]">
+          <span className="text-[#ff8906] text-xl font-medium ">
             {price}
           </span>
         </div>
         <footer className="flex items-center gap-2">
           <div className="flex w-2/3">
-            <Button text="Buy">Buy</Button>
+            <Button text="Buy" ></Button>
           </div>
           <a href="#" className="flex w-1/3 flex items-center justify-center border border-[#ff8906] rounded-md p-1">
             <img src="src/assets/icons/ShoppingCartOren.svg" alt="icon-cart-shopping" className="w-6 h-6" />
