2e2d29dfbafa00f60c0d2722ef0b3046c85434c6
mslthn <msulthon1909@gmail.com>
2026-02-01 22:47:49 +0700

    feat: add register link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const NavigationBar = () => {
                     <div className="text-white border border-white h-12 w-20 flex items-center justify-center rounded-lg">
                         <Link to="/login">Sign In</Link></div>
                     <div>
-                        <Link to="/register" className="bg-[#FF8906] h-12 w-20 flex items-center justify-center rounded-lg">Sign Up</Link>
+                        <Link to="/register" className="bg-[#FF8906] h-12 w-20 flex items-center justify-center rounded-lg hover:bg-orange-500">Sign Up</Link>
                     </div>
                 </div>
             </nav>
