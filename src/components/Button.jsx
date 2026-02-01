a4d7ce2ca2ea2b42f27b0abdcb6128ecb98444ad
mslthn <msulthon1909@gmail.com>
2026-02-01 22:46:35 +0700

    feat: add hover and cursor pointer to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = (props) => {
     const {type, text, onClick} = props
     return (
-        <button className="w-full bg-orange-400 p-1 rounded-lg" 
+        <button className="w-full bg-orange-400 p-1 rounded-lg cursor-pointer hover:bg-orange-500" 
             type={type} onClick={onClick}>{text}</button>
     )
 }
