3ae093d7cdfbc16717b44680d02e4c2d049d2046
mslthn <msulthon1909@gmail.com>
2026-02-01 18:24:09 +0700

    refactor: create outlet and use main layout in several pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,62 @@
-import {createBrowserRouter, RouterProvider, } from 'react-router-dom'
-import HomePage from "./pages/HomePage"
-import RegisterPage from './pages/Register'
-import LoginPage from './pages/Login'
-import ForgotPasswordPage from './pages/ForgotPassword'
-import ProductsPage from './pages/ProductsPage'
-import DetailProductsPage from './pages/DetailProductsPage'
-import CheckoutProductPage from './pages/CheckoutProductPage'
-import OrderHistory from './pages/OrderHistory'
-import OrderDetail from './pages/OrderDetailPage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './layout/MainLayout';
+import HomePage from './pages/HomePage';
+import RegisterPage from './pages/Register';
+import LoginPage from './pages/Login';
+import ForgotPasswordPage from './pages/ForgotPassword';
+import ProductsPage from './pages/ProductsPage';
+import DetailProductsPage from './pages/DetailProductsPage';
+import CheckoutProductPage from './pages/CheckoutProductPage';
+import OrderHistory from './pages/OrderHistory';
+import OrderDetail from './pages/OrderDetailPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <HomePage/>,
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'forgotPassword',
+        element: <ForgotPasswordPage />,
+      },
+      {
+        path: 'products',
+        element: <ProductsPage />,
+      },
+      {
+        path: 'detailProduct',
+        element: <DetailProductsPage />,
+      },
+      {
+        path: 'checkoutProduct',
+        element: <CheckoutProductPage />,
+      },
+      {
+        path: 'orderHistory',
+        element: <OrderHistory />,
+      },
+      {
+        path: 'orderDetail',
+        element: <OrderDetail />,
+      },
+    ],
   },
-  {
-    path: "/register",
-    element: <RegisterPage/>
-  },
-  {
-    path: "/login",
-    element: <LoginPage/>
-  },
-  {
-    path: "/forgotPassword",
-    element: <ForgotPasswordPage/>
-  },
-  {
-    path: "/products",
-    element: <ProductsPage/>
-  },
-  {
-    path: "/detailProduct",
-    element: <DetailProductsPage/>
-  },
-  {
-    path: "/checkoutProduct",
-    element: <CheckoutProductPage/>
-  },
-  {
-    path: "/orderHistory",
-    element: <OrderHistory/>
-  },
-  {
-    path: "/orderDetail",
-    element: <OrderDetail/>
-  }
-])
+]);
 
 function App() {
-  return (
-    <RouterProvider router={router}></RouterProvider>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
