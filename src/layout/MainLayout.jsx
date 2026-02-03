de4e7eded9804270cb54b5822dc079aef27e10c6
mslthn <msulthon1909@gmail.com>
2026-02-03 09:20:44 +0700

    feat: add main layout component to set the main layout and use outlet

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,14 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import NavigationBar from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
+import { useEffect, useState } from 'react';
+import fetchMenuData from '../lib/fetchMenu.js';
 
 const Layout = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+    (async()=>{
+      const data = await(fetchMenuData)
+      setProducts(products)
+    })
+
+  },[])
+
   return (
     <div className="flex flex-col min-h-screen">
       <NavigationBar />
-      <main className="flex-grow">
+      {/* <main className="flex-grow"> */}
         <Outlet />
-      </main>
+      {/* </main> */}
       <Footer />
     </div>
   );
