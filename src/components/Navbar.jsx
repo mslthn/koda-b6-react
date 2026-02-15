0a53971b1dc0a2ab61f2e2dde1197dc516cc9eff
mslthn <msulthon1909@gmail.com>
2026-02-15 23:49:02 +0700

    feat: implementing context to display user name when user login and remove when user logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom"
+import { use, useContext } from "react"
+import { Link, Navigate, useNavigate } from "react-router-dom"
+import UserContext from "./context/UserContext"
 
 const NavigationBar = () => {
+    const navigate = useNavigate()
+    const user = useContext(UserContext)
+
+    function logOut(){
+        localStorage.removeItem("loggedInUser")
+        navigate("/login")
+    }
+
     return (
         <div className="fixed w-full z-5">
             <nav className="flex flex-row justify-around bg-[#0B090921] p-5 items-center">
@@ -21,11 +31,23 @@ const NavigationBar = () => {
                 <div className="flex flex-row gap-8 items-center">
                     <div><img src="./src/assets/icons/Search.svg" alt="Search Icon" /></div>
                     <div><img src="./src/assets/icons/ShoppingCart.svg" alt="Shoping cart" /></div>
-                    <div className="text-white border border-white h-12 w-20 flex items-center justify-center rounded-lg">
-                        <Link to="/login">Sign In</Link></div>
-                    <div>
-                        <Link to="/register" className="bg-[#FF8906] h-12 w-20 flex items-center justify-center rounded-lg hover:bg-orange-500">Sign Up</Link>
-                    </div>
+                    { user ? 
+                        <>
+                            <button className="text-black border border-white h-12 w-20 flex items-center justify-center rounded-lg">
+                                {user.fullname}
+                            </button>
+                            <button onClick={()=>{logOut()}}>Log out</button>
+                        </>
+                        :
+                        <>
+                            <div className="text-white border border-white h-12 w-20 flex items-center justify-center rounded-lg">
+                                <Link to="/login">Sign In</Link>
+                            </div>
+                            <div>
+                                <Link to="/register" className="bg-[#FF8906] h-12 w-20 flex items-center justify-center rounded-lg hover:bg-orange-500">Sign Up</Link>
+                            </div>
+                        </>
+                    }
                 </div>
             </nav>
         </div>
