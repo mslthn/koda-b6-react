37f88f25b1f3931193a80160f1f78d55731bbada
mslthn <msulthon1909@gmail.com>
2026-04-05 23:41:30 +0700

    chore: add query client into main

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Opsional: Atur agar tidak re-fetch otomatis saat pindah tab
+      refetchOnWindowFocus: false,
+    },
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
