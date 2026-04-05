4a7d1a0385fe212d144f8b383764b30c5ea77e99
mslthn <msulthon1909@gmail.com>
2026-04-05 23:17:00 +0700

    feat: create axios instance

---
+++ src/lib/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8888',
+  
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  
+  // Timeout jika request memakan waktu terlalu lama (misal 10 detik)
+  timeout: 10000, 
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
