5ac3d45593a4c404ad08f1da18e2a576d7973f46
mslthn <msulthon1909@gmail.com>
2026-04-06 10:53:46 +0700

    chore: update eslint config on defineConfig to solve chunk issue

--- eslint.config.js
+++ eslint.config.js
@@ -25,5 +25,16 @@ export default defineConfig([
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
+    build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
   },
 ])
