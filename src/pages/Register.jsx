a0de2c2dab653fa9cf464b148c3f2334bc2348b9
mslthn <msulthon1909@gmail.com>
2026-04-06 11:01:02 +0700

    chore: import useState library

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
 import api from "../lib/http.js"
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from 'yup';
